ac3261b98992eb0ba1067830ad92703625abe67f
Sam Searle <samsearle66@gmail.com>
2017-10-24 15:49:16 +1300

    tidy up time

--- WildernessWine/src/com/redeyed600/bots/wilderness_wine/GC.java
+++ WildernessWine/src/com/redeyed600/bots/wilderness_wine/GC.java
@@ -1,9 +1,16 @@
 package com.redeyed600.bots.wilderness_wine;
 
+import com.runemate.game.api.hybrid.location.Area;
+import com.runemate.game.api.hybrid.location.Coordinate;
+
 public class GC {
 
     public static int LAWRUNES = 26;
     public static int FIRERUNES = 1;
     public static Boolean AIRSTAFF = true;
+    //public static int\
+    public static Coordinate edgevilleBank = new Coordinate(3095,3517,0);
+    public final Area.Circular alter = new Area.Circular(new Coordinate(2950,3821,0),2);
+    public final Area.Circular northOfDitch = new Area.Circular(new Coordinate(3070,3535,0),4);
 
 }
