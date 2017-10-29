cd9bb23d9a78bc30fcd4f504e402fe6e5e6681a6
Sam Searle <samsearle66@gmail.com>
2017-10-29 22:05:08 +1300

    fuckedpc

--- WildernessWine/src/com/redeyed600/bots/wilderness_wine/wilderness_wine.java
+++ WildernessWine/src/com/redeyed600/bots/wilderness_wine/wilderness_wine.java
@@ -11,10 +11,10 @@ public class wilderness_wine extends TaskBot {
         setLoopDelay(700,1200);
 
         //add(new Task1(this));
-        add(new Heal(this), new WalkToBank(this), new BankInterface(this), new WalkToNorthOfDitch(this), new WalkToLevel20Wilderness(this), new WalkToAlterDoor(this), new WalkToAlter(this), new GrabWine(this), new WalkToAlterDoor(this), new WalkToLevel20Wilderness(this), new WalkToNorthOfDitch(this), new WalkToSouthOfDitch(this));
+        //add(new Heal(this), new WalkToBank(this), new BankInterface(this), new WalkToNorthOfDitch(this), new WalkToLevel20Wilderness(this), new WalkToAlterDoor(this), new WalkToAlter(this), new GrabWine(this), new WalkToAlterDoor(this), new WalkToLevel20Wilderness(this), new WalkToNorthOfDitch(this), new WalkToSouthOfDitch(this));
         //add(new WalkToBank(), new BankInterface());
         //add(new GrabWine());
-        //add(new Task1());
+        add(new Bank());
 
     }
 }
