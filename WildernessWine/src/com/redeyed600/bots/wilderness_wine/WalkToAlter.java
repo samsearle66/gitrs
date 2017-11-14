4864bcbfdf7839a5dc19a115d31228f177d5a486
unknown <samsearle66@gmail.com>
2017-11-14 16:36:37 -0600

    fixed door and pathing from v to e

--- WildernessWine/src/com/redeyed600/bots/wilderness_wine/WalkToAlter.java
+++ WildernessWine/src/com/redeyed600/bots/wilderness_wine/WalkToAlter.java
@@ -35,7 +35,7 @@ public class WalkToAlter extends Task {
         final BresenhamPath path = BresenhamPath.buildTo(ww.alter);
         if (path != null) { // Although BresenhamPath technically always builds a path, it is recommended to nullcheck rather than having the bot crash
             if(!ww.alter.contains(me)) {
-                if(door!=null && door.isValid() && door.isVisible())
+                if(door!=null && door.isValid() && door.isVisible() && ww.alterDoorArea.contains(me))
                     add(new IsDoorOpen(ww));
                 else
                     path.step();
--- WildernessWine/src/com/redeyed600/bots/wilderness_wine/WalkToAlterDoor.java
+++ WildernessWine/src/com/redeyed600/bots/wilderness_wine/WalkToAlterDoor.java
@@ -71,7 +71,7 @@ public class WalkToAlterDoor extends Task {
             if (path != null) { // Although BresenhamPath technically always builds a path, it is recommended to nullcheck rather than having the bot crash
 
                 if (!alterDoor.contains(me)) {
-                    if (door != null && door.isValid() && door.isVisible())
+                    if (door != null && door.isValid() && door.isVisible() && ww.alterDoorArea.contains(me))
                         add(new IsDoorOpen(ww));
                     else
                         path.step();
